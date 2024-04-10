313d4d7bff6d1f0b18c5d940177cc5de0060de35
Ajinis <164571898+Ajiniss@users.noreply.github.com>
2024-04-10 21:04:25 +0200

    Delete wal directory

--- wal/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#f2e6e3", "#291318" },
-	[SchemeSel] = { "#f2e6e3", "#C7A29A" },
-	[SchemeOut] = { "#f2e6e3", "#F8D1D0" },
-};
--- wal/colors-wal-tabbed.h
+++
@@ -1,6 +0,0 @@
-static const char* selbgcolor   = "#291318";
-static const char* selfgcolor   = "#f2e6e3";
-static const char* normbgcolor  = "#C9AEAE";
-static const char* normfgcolor  = "#f2e6e3";
-static const char* urgbgcolor   = "#C7A29A";
-static const char* urgfgcolor   = "#f2e6e3";
