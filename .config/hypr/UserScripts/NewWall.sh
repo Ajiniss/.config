aa1b8cdc55eda1da05d175b5b5c643317f92c222
ajinis <baptistekenobi@gmail.com>
2024-04-11 13:44:56 +0200

    Henri commit

--- .config/hypr/UserScripts/NewWall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-substitue_string=$(echo "$1" | sed 's/....$//')
-
-mogrify -format png $1
-mv "$substitue_string.png" ~/wallpaper/$2.png
--- .config/hypr/UserScripts/uskeyboard.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-default_layout="us"
